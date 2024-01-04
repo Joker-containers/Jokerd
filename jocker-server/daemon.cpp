6629733ab66012b2d1758ca718a4ba45303ee5b9
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2024-01-04 15:09:13 +0200

    Receiving + saving container files works

--- jocker-server/daemon.cpp
+++ jocker-server/daemon.cpp
@@ -1,35 +1,64 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <vector>
 #include <netinet/in.h>
 #include <unistd.h>
 
-constexpr int PORT = 12345;
+const int PORT = 12345; // Replace with your desired port number
 
-void receiveFile(int clientSocket, const std::string& filePath) {
-    std::ofstream fileStream(filePath, std::ios::binary);
-    if (!fileStream.is_open()) {
-        std::cerr << "Error: Unable to open file for writing: " << filePath << std::endl;
+void receiveFileandConfig(int clientSocket) {
+    std::cout << "\nStarting to receive container files...\n" << std::endl;
+
+    std::cout << "\nReceiving binary filename size..." << std::endl;
+    uint64_t binary_name_size;
+    recv(clientSocket, &binary_name_size, sizeof(binary_name_size), 0);
+    std::cout << "Received binary filename length: " << binary_name_size << std::endl;
+
+    std::cout << "\nReceiving binary filename..." << std::endl;
+    std::vector<char> binary_name(binary_name_size);
+    recv(clientSocket, binary_name.data(), binary_name_size, 0);
+    std::cout << "Received binary filename: " << binary_name.data() << std::endl;
+
+    std::cout << "\nReceiving binary size..." << std::endl;
+    uint64_t binary_size;
+    recv(clientSocket, &binary_size, sizeof(binary_size), 0);
+    std::cout << "Received binary size: " << binary_size << std::endl;
+
+    std::cout << "\nReceiving binary..." << std::endl;
+    std::vector<char> binary(binary_size);
+    recv(clientSocket, binary.data(), binary_size, 0);
+    std::cout << "Received binary." << std::endl;
+
+    std::string config_filename = std::string(binary_name.data()) + ".joker";
+    std::cout << "\nConfig filename is: " << config_filename << std::endl;
+    std::cout << "Receiving config size..." << std::endl;
+    uint64_t binary_config_size;
+    recv(clientSocket, &binary_config_size, sizeof(binary_config_size), 0);
+    std::cout << "Received config size: " << binary_config_size << std::endl;
+
+    std::cout << "\nReceiving config..." << std::endl;
+    std::vector<char> binary_config(binary_config_size);
+    recv(clientSocket, binary_config.data(), binary_config_size, 0);
+    std::cout << "Received config." << std::endl;
+
+    std::ifstream check_file(binary_name.data());
+    if (check_file.good()) {
+        std::cerr << "\nError: File with the name " << binary_name.data() << " already exists." << std::endl;
         return;
     }
+    check_file.close();
 
-    char buffer[1024];
-    ssize_t bytesRead;
-    size_t totalBytesReceived = 0;
+    std::cout << "\nSaving binary file..." << std::endl;
+    std::ofstream binary_file(binary_name.data(), std::ios::binary);
+    binary_file.write(binary.data(), binary_size);
+    binary_file.close();
+    std::cout << "Saved binary file to " << binary_name.data() << ".\n" << std::endl;
 
-    while ((bytesRead = read(clientSocket, buffer, sizeof(buffer))) > 0) {
-        fileStream.write(buffer, bytesRead);
-        totalBytesReceived += bytesRead;
-    }
-
-    fileStream.close();
-
-    if (bytesRead == -1) {
-        std::cerr << "Error occurred during file transfer." << std::endl;
-    } else {
-        std::cout << "File received: " << filePath << std::endl;
-        std::cout << "Size of the file: " << totalBytesReceived << " bytes" << std::endl;
-    }
+    std::cout << "\nSaving config file..." << std::endl;
+    std::ofstream binary_config_file(config_filename, std::ios::binary);
+    binary_config_file.write(binary_config.data(), binary_config_size);
+    binary_config_file.close();
+    std::cout << "Saved config file to " << config_filename << ".\n" << std::endl;
 }
 
 int main() {
@@ -44,7 +73,7 @@ int main() {
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(PORT);
 
-    if (bind(serverSocket, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr)) == -1) {
+    if (bind(serverSocket, reinterpret_cast<struct sockaddr *>(&serverAddr), sizeof(serverAddr)) == -1) {
         std::cerr << "Error: Unable to bind socket." << std::endl;
         close(serverSocket);
         return 1;
@@ -62,25 +91,13 @@ int main() {
         sockaddr_in clientAddr{};
         socklen_t clientAddrLen = sizeof(clientAddr);
 
-        int clientSocket = accept(serverSocket, reinterpret_cast<struct sockaddr*>(&clientAddr), &clientAddrLen);
+        int clientSocket = accept(serverSocket, reinterpret_cast<struct sockaddr *>(&clientAddr), &clientAddrLen);
         if (clientSocket == -1) {
             std::cerr << "Error: Unable to accept client connection." << std::endl;
             continue;
         }
 
-//        char filePathBuffer[1024];
-//        ssize_t filePathLength = read(clientSocket, filePathBuffer, sizeof(filePathBuffer));
-//        if (filePathLength <= 0) {
-//            std::cerr << "Error: Unable to read file path from client." << std::endl;
-//            close(clientSocket);
-//            continue;
-//        }
-//
-//        filePathBuffer[filePathLength] = '\0';
-//        std::string filePath(filePathBuffer);
-//
-//        receiveFile(clientSocket, filePath);
-        receiveFile(clientSocket, "saved_file.json");
+        receiveFileandConfig(clientSocket);
 
         close(clientSocket);
     }
