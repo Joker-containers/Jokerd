1946a991bedd6db1d174eb4f3a35022aa1632600
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2024-01-07 21:14:14 +0200

    recv_all and send_all implemented

--- jocker-server/other/common.cpp
+++ jocker-server/other/common.cpp
@@ -1,6 +1,9 @@
 #include "common.h"
 #include "syscall_wrapper.h"
 
+const size_t BUFFER_SIZE = 1024;
+
+
 void deleteCharPtrArray(char **av) {
     if (av) {
         for (size_t i = 0; av[i]; ++i) {
@@ -10,8 +13,8 @@ void deleteCharPtrArray(char **av) {
     }
 }
 
-std::unique_ptr<char *[], void(*)(char **)> createCharPtrArray(const std::vector<std::string> &tokens) {
-    std::unique_ptr<char *[], void(*)(char **)> ptr(new char *[tokens.size() + 1], deleteCharPtrArray);
+std::unique_ptr<char *[], void (*)(char **)> createCharPtrArray(const std::vector<std::string> &tokens) {
+    std::unique_ptr<char *[], void (*)(char **)> ptr(new char *[tokens.size() + 1], deleteCharPtrArray);
     ptr[tokens.size()] = nullptr;
     for (size_t i = 0; i < tokens.size(); ++i) {
         ptr[i] = new char[tokens[i].length() + 1];
@@ -20,23 +23,61 @@ std::unique_ptr<char *[], void(*)(char **)> createCharPtrArray(const std::vector
     return ptr;
 }
 
-struct raii_fd{
+struct raii_fd {
     int fd;
 
-    explicit raii_fd(int fd): fd(fd){};
-    ~raii_fd(){
+    explicit raii_fd(int fd) : fd(fd) {};
+
+    ~raii_fd() {
         syscall_wrapper(close, "close", fd);
     }
 };
 
-int write_to_file(const std::string &path, const std::string &content){
+int write_to_file(const std::string &path, const std::string &content) {
     raii_fd fd = raii_fd(syscall_wrapper(open, "open", path.c_str(), O_RDWR));
     off_t content_ptr = 0;
-    while (true){
-        content_ptr += syscall_wrapper(write, "write", fd.fd, content.c_str() + content_ptr, content.size() - content_ptr);
-        if (content_ptr == content.size()){
+    while (true) {
+        content_ptr += syscall_wrapper(write, "write", fd.fd, content.c_str() + content_ptr,
+                                       content.size() - content_ptr);
+        if (content_ptr == content.size()) {
             break;
         }
     }
     return 0;
+}
+
+
+void recv_all(int socket, char *buffer, size_t size) {
+    ssize_t bytesRead;
+
+    while (size > 0) {
+        bytesRead = recv(socket, buffer, BUFFER_SIZE, 0);
+        if (bytesRead == -1) {
+            perror("Error in recv");
+        }
+        size -= bytesRead;
+    }
+
+    if (bytesRead == 0) {
+        std::cout << "Connection closed by the peer." << std::endl;
+    } else if (bytesRead == -1) {
+        perror("Error in recv");
+    }
+}
+
+void send_all(int socket, const char *buffer, size_t size) {
+    ssize_t bytesSent;
+
+    while (size > 0) {
+        bytesSent = send(socket, buffer, BUFFER_SIZE, 0);
+        if (bytesSent == -1) {
+            perror("Error in send");
+        }
+        size -= bytesSent;
+        buffer += bytesSent;
+    }
+
+    if (bytesSent == -1) {
+        perror("Error in send");
+    }
 }
--- jocker-server/other/common.h
+++ jocker-server/other/common.h
@@ -7,14 +7,17 @@
 #include <sstream>
 #include <fcntl.h>
 #include <iostream>
+#include <sys/socket.h>
 
 struct raii_fd;
 
 void deleteCharPtrArray(char **av);
 
-std::unique_ptr<char *[], void(*)(char **)> createCharPtrArray(const std::vector<std::string> &tokens);
+std::unique_ptr<char *[], void (*)(char **)> createCharPtrArray(const std::vector<std::string> &tokens);
 
 int write_to_file(const std::string &path, const std::string &content);
+
+void recv_all(int socket, char *buffer, size_t size);
 
 
 #endif //JOCKER_SERVER_COMMON_H
